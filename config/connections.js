ee07d6d36aa46d4a8cd87cd2a2e118590ba9656d
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-07-21 08:24:51 -0400

    fixed error

--- config/connections.js
+++ config/connections.js
@@ -7,13 +7,14 @@ if(process.env.JAWSDB_URL){
 }
 else{
   connection = mysql.createConnection({
-  host: process.env.DBHOST || "localhost", //127.0.0.1 ip address
-  user: process.env.DBUSER || "root",
-  password: process.env.DBPASSWORD || "root",
-  port: 3306, //3306 default port
-  database: process.env.DB || "burgers_db",
-  multipleStatements: true
-});
+    host: process.env.DBHOST || "localhost", //127.0.0.1 ip address
+    user: process.env.DBUSER || "root",
+    password: process.env.DBPASSWORD || "root",
+    port: 3306, //3306 default port
+    database: process.env.DB || "burgers_db",
+    multipleStatements: true
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
